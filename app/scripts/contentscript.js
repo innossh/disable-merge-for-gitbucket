0bafeac5d5213d6269bc7c3294d3d9e62edf8ca0
innossh <innossh@users.noreply.github.com>
2014-07-27 21:27:44 +0900

    Disable GitBucket Pull Request close button

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -6,7 +6,16 @@
         var mergeButtonHtml = '<span class="btn disabled">You can\'t merge!</span>';
         $mergeButton.html(mergeButtonHtml);
     };
+    var disableCloseButton = function() {
+        var $closeButton = $('#action');
+        var $parent = $closeButton.closest('div');
+        $closeButton.remove();
+        var closeButtonHtml = '<span class="btn disabled">You can\'t close!</span>';
+        $parent.append(closeButtonHtml);
+    };
 
     disableMergeButton();
+    disableCloseButton();
     setInterval(disableMergeButton, 1000);
+    setInterval(disableCloseButton, 1000);
 })(jQuery);
